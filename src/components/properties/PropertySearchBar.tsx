62f7aba02cfc561f1670d70bda859f8a72b39d29
Gaston Roxas <gacaro51597@gmail.com>
2023-03-12 14:30:05 +0800

    fix search button on navbar:'

--- src/components/properties/PropertySearchBar.tsx
+++ src/components/properties/PropertySearchBar.tsx
@@ -126,7 +126,12 @@ const PropertySearchBar = () => {
         sx={{ maxWidth: 170 }}
       />
 
-      <Button onClick={onSubmit}>Search</Button>
+      <Button
+        className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        onClick={onSubmit}
+      >
+        Search
+      </Button>
     </Box>
   );
 };
